873d874156dce71ddaae77ce3f608db488333b0b
Aman srivastava <srivastavaaman085@gmail.com>
2023-01-19 08:23:41 +0530

    Create Macro.cpp

---
+++ Macro.cpp
@@ -0,0 +1,13 @@
+
+#define int long long
+#define ll long long
+#define W(a,b) while(a<b)
+#define A(n) vector<ll>A
+#define s(a) sort(a.begin(),a.end())
+#define read(n) cin>>n;
+#define Yes cout<<"Yes"<<"\n"
+#define No cout<<"No"<<"\n"
+#define Rep(i,a,b) for(int i=a;i<b;i++)
+#define Rep(j,a,b) for(int j=a;j<b;j++)
+#define print(n) cout<<n<<"\n";
+//#define Rep(i,0,n) for(int i=a;i<n;i++)
