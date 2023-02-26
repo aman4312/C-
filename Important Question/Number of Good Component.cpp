6e34b71b7c1c31626c75ee28c0de40d52bab5468
Aman srivastava <srivastavaaman085@gmail.com>
2023-02-26 20:38:37 +0530

    Create Number of Good Component.cpp

---
+++ Important Question/Number of Good Component.cpp
@@ -0,0 +1,35 @@
+
+// using dfs count the vertex and edges 
+class Solution {
+  public:
+  vector<int>vis;
+  void dfs(int source, vector<vector<int>>&adj, int &vertex, int &edges)
+  {
+      vis[source]=1;
+      vertex++;
+      for(auto child: adj[source])
+      {
+          edges++;
+          if(!vis[child])
+          {
+              dfs(child, adj, vertex, edges);
+          }
+      }
+  }
+    int findNumberOfGoodComponent(int V, vector<vector<int>>& adj) {
+        // code here
+        vis=vector<int>(V+1, 0);
+        int ans=0;
+        for(int i=1;i<=V;i++)
+        {
+            if(!vis[i])
+            {
+                int vertex=0,edges=0;
+                
+                dfs(i,adj,vertex,edges);
+                edges/=2;
+                if(edges==(vertex*(vertex-1)/2))ans++;
+            }
+        }
+        return  ans;
+    }
